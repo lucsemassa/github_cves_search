59e388c0b923906f48e76a61b12422f74d9e1706
lucsemassa <40078459+lucsemassa@users.noreply.github.com>
2023-09-21 05:56:36 -0400

    linux kernel cves search

---
+++ cves_search.py
@@ -0,0 +1,103 @@
+import requests
+import json
+import sys
+import time
+from bs4 import BeautifulSoup
+
+
+
+cves = []
+
+def search_cve(link):
+    global cves
+    response = requests.get(link)
+    if response.status_code == 200:
+        # Parse the HTML content of the response
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        matching_records_count = int(soup.find('strong', {'data-testid': 'vuln-matching-records-count'}).text)
+        displaying_count_through = int(soup.find('strong', {'data-testid': 'vuln-displaying-count-through'}).text)
+
+        # Extract CVEs
+        
+        for result in soup.find_all(lambda tag: tag.name == 'a' and tag.get('data-testid', '').startswith('vuln-detail-link-')):
+            cve = result.text.strip()
+            cves.append(cve)
+    else:
+        return None
+
+    return [matching_records_count, displaying_count_through]
+
+def search_github(cve_search):
+    # Set the GitHub API URL
+    api_url = "https://api.github.com/search/repositories?q="+cve_search
+
+    # Define headers (optional, you can include your token here)
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        # "Authorization": f"Bearer {GITHUB_API_TOKEN}",  # Uncomment and replace with your token
+    }
+
+    # Send the GET request to the GitHub API
+    cve_response = requests.get(api_url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if cve_response.status_code == 200:
+        # Parse the JSON response
+        data = json.loads(cve_response.text)
+        
+        # Extract the 'html_url' for each matching repository
+        for item in data.get("items", []):
+            html_url = item.get("html_url")
+            if html_url:
+                print(html_url)
+
+        time.sleep(10)
+    else:
+        print("")
+        
+
+
+def main():
+    global kernel_version, cves
+    # Send an HTTP GET request to the URL
+    # Define the URL with the kernel version (e.g., 5.15.70)
+   
+   
+
+    # Check if a search query is provided as a command-line argument
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <search_query>")
+        sys.exit(1)
+
+    # Get the search query from the command-line argument
+    kernel_version = sys.argv[1]
+    url = f"https://nvd.nist.gov/vuln/search/results?isCpeNameSearch=false&cpe_vendor=cpe%3A%2F%3Alinux&cpe_version=cpe%3A%2F%3Alinux%3Alinux_kernel%3A{kernel_version}&query=escalate&cpe_product=cpe%3A%2F%3Alinux%3Alinux_kernel&results_type=overview&form_type=Advanced&search_type=all&startIndex="
+
+    get_first_cve = search_cve(url+"0")
+
+    if(get_first_cve ==  None):
+        print("Failed to retrieve data. Status code:", response.status_code)
+        exit()
+
+
+    matching_records_count = get_first_cve[0]
+    displaying_count_through = get_first_cve[1]
+    
+
+    remainder = 1 if (matching_records_count % displaying_count_through != 0 and matching_records_count > displaying_count_through) else 0
+
+    for i in range(1, int(matching_records_count/displaying_count_through)+remainder+1):
+        search_cve(url+str(i*20))
+
+    if cves:
+        print("CVEs related to Linux Kernel", kernel_version)
+        for cve in cves:
+            print(cve)
+            search_github(cve.replace(" ", ""))
+            #print("")
+
+    else:
+        print("No CVEs found for the specified kernel version.")
+
+main()
